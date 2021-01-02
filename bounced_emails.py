1df2758c3d21e12ae2ef1eeae2b88992735213ee
Ryan Alamo <ralamo@pop-os.localdomain>
2021-01-02 17:07:40 -0500

    Works with hardcoded email address

---
+++ bounced_emails.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('EmailList')
+
+
+def lambda_handler(event, context):
+    print("starting")
+    email = extractEmailAddress(event)
+    print("Deleting " + email + " from the EmailList table")
+    table.delete_item(
+        Key={
+            "Email": email
+        }
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(email + ' was deleted from the EmailList table')
+    }
+
+
+def extractEmailAddress(event):
+    return "ralamo33@gmail.com";
+
+if __name__ == "__main__":
+    lambda_handler("h", "c")
